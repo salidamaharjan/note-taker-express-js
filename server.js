444586894379f90c25a4d34b41c9b5d259bd7663
salidamaharjan <salidamaharjan@outlook.com>
2024-01-22 15:57:36 -0600

    read the database object and send as JSON object

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const fsPromises = require("fs").promises;
+const path = require("path");
+
 const app = express();
 const PORT = 3000;
 
 // using middle ware
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/notes', (req, res) => 
-res.sendFile(path.join(__dirname, 'public', 'notes.html')));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
 
+// route used to read the database object and serve as JSON object
+app.get('/api/notes', async (req, res) => {
+  const fileData = await fsPromises.readFile('./db/db.json', 'utf-8');
+  const jsonData = JSON.parse(fileData);
+  res.json(jsonData);
+})
 
 // starting the server at PORT 3000;
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
